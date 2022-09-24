5459db93a3375759c4782aad607cdf5c5347bd12
Chris Judd <ipajudd@gmail.com>
2022-09-23 18:46:49 -0700

    chore: dark mode config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,10 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+      },
       navbar: {
         title: 'IT Handbook',
         logo: {
